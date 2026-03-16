60fdbd5176e619d4873d900436ef631bd30ba73f
kimMinSu0304 <ainsopor0304@gmail.com>
2026-03-16 10:50:32 +0900

    무기해금 텍스트 추가
    무기해금 텍스트 추가

---
+++ Source/Flip_Side/UI/ShopUnlockWeapon/W_UnlockWeaponExplainWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/W_ExplainWidget.h"
+#include "W_UnlockWeaponExplainWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLIP_SIDE_API UW_UnlockWeaponExplainWidget : public UW_ExplainWidget
+{
+	GENERATED_BODY()
+	
+};
