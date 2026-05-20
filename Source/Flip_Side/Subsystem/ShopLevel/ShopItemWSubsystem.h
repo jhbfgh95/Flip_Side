0e31ca3444275162cc6299ff364ef8ee3c034f93
kimMinSu0304 <ainsopor0304@gmail.com>
2026-05-20 22:24:50 +0900

    상점 서브시스템 및 ? 버튼 추가
    상점 아이템 버그 개선
    ? 버튼 추가

--- Source/Flip_Side/Subsystem/ShopLevel/ShopItemWSubsystem.h
+++ Source/Flip_Side/Subsystem/ShopLevel/ShopItemWSubsystem.h
@@ -34,7 +34,7 @@ protected:
 
 	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
-
+	virtual void OnWorldBeginPlay(UWorld& InWorld) override;
 
 private:
 	class UDataManagerSubsystem* DM;
